116100515b1764b19ca9c16b72ffc0c5e58c0b4b
Bradley J. Tannor <bradleytannor@gmail.com>
2020-04-19 11:27:39 -0700

    Prepare Deployment
    Fixed 'require' statement.

--- lib/node_adapter_impl.rb
+++ lib/node_adapter_impl.rb
@@ -1,7 +1,7 @@
 # Copyright (C) 2020 Diligent Software LLC. All rights reserved. Released
 # under the GNU General Public License, Version 3. Refer LICENSE.txt.
 
-require_relative "node_adapter_impl/version"
+require_relative 'node_adapter_impl/version'
 
 # NodeAdapter.
 # @class_description
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,4 @@
 $LOAD_PATH.unshift File.expand_path("../lib", __dir__)
-require "node_adapter_impl"
 
-require "minitest/autorun"
+require_relative '../lib/node_adapter_impl'
+require 'minitest/autorun'
