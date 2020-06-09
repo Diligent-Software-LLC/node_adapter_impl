cd41434b019e86f90a067826c889d818b9b2ce8b
Bradley J. Tannor <bradleytannor@gmail.com>
2020-06-09 10:30:56 -0700

    Prepare Deployment
    Testing dependency bug.

--- lib/node_adapter_impl/version.rb
+++ lib/node_adapter_impl/version.rb
@@ -1,7 +1,7 @@
 # Copyright (C) 2020 Diligent Software LLC. All rights reserved. Released
 # under the GNU General Public License, Version 3. Refer LICENSE.txt.
 
-#require 'node_comp'
+require 'node_comp'
 
 # NodeAdapter.
 # @class_description
@@ -12,6 +12,6 @@
 #   A data instance reference.
 # @attr front [NodeAdapter, NilClass]
 #   A forward reference.
-class NodeAdapter #< Node
+class NodeAdapter < Node
   VERSION = '1.0.0'.freeze()
 end
